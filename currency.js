4314d0cb25ac3b3a454a0304ff0e7fc5f9df514d
Mike Cornish <cornishmw@gmail.com>
2017-02-05 17:41:48 -0500

    Adds test conversion

---
+++ currency.js
@@ -0,0 +1,19 @@
+const fetch = require('whatwg-fetch')
+const key = process.env.CURRENCY_LAYER_KEY
+
+module.exports = {
+  convert
+}
+
+function convert (from, to, amount, cb) {
+  const currencyString = `currencies=${from},${to}`
+  const url = `http://apilayer.net/api/live?access_key=${key}&${currencyString}`
+
+  const promise = fetch(url)
+
+  if (cb) {
+    promise.then(cb)
+  }
+
+  return promise
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 const send = require('./send')
 const thread = require('./thread')
+const currency = require('./currency')
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -37,8 +38,10 @@ app.post('/webhook/', (req, res) => {
     const sender = event.sender.id
     if (event.message && event.message.text) {
       const text = event.message.text
-      if (text.toLowerCase().indexOf('generic') > -1) {
-        send.genericMessage(sender)
+      if (text.toLowerCase().indexOf('convert') > -1) {
+        currency.convert('USD', 'CAD', 5).then(res => {
+          send.textMessage(sender, 'Found: ' + res.quotes[`USDCAD`])
+        })
       } else if (event.postback) {
         const text = JSON.stringify(event.postback)
         send.textMessage(sender, 'Postback received: ' + text.substring(0, 200), token)
